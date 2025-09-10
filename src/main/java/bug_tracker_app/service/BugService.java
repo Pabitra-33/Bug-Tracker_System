7b3ce86f5e9075ae4250e43f3ad01208d12c9080
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-10 22:24:57 +0530

    created the implementation class to provide the abstract method implementation

--- src/main/java/bug_tracker_app/service/BugService.java
+++ src/main/java/bug_tracker_app/service/BugService.java
@@ -9,6 +9,6 @@ public interface BugService {
 	//created abstract methods to achieve abstraction
 	public Bug addBug(Bug bug);//to add new bugs
 	public List<Bug> getAllBugs();//to get all bugs
-	public Bug updateBug(Long id, Bug bug);//to update existing bugs
+	public Bug updateBug(Long id, Bug bugDetails);//to update existing bugs
 	public void deleteBug(Long id);//to delete bugs
 }
---
+++ src/main/java/bug_tracker_app/service/BugServiceImplementation.java
@@ -0,0 +1,45 @@
+package bug_tracker_app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bug_tracker_app.entity.Bug;
+import bug_tracker_app.repository.BugRepository;
+
+@Service
+public class BugServiceImplementation implements BugService {
+
+	@Autowired
+	private final BugRepository bugRepository;
+	
+	public BugServiceImplementation(BugRepository bugRepository) {
+		this.bugRepository = bugRepository;
+	}
+	
+	@Override
+	public Bug addBug(Bug bug) {
+		return bugRepository.save(bug);
+	}
+
+	@Override
+	public List<Bug> getAllBugs() {
+		return bugRepository.findAll();
+	}
+
+	@Override
+	public Bug updateBug(Long id, Bug bugDetails) {
+		Bug bug = bugRepository.findById(id).orElseThrow(() -> new RuntimeException("Bug id not found"));
+		bug.setTitle(bug.getTitle());
+		bug.setDescription(bug.getDescription());
+		bug.setStatus(bug.getStatus());
+		
+		return bugRepository.save(bug);
+	}
+
+	@Override
+	public void deleteBug(Long id) {
+		bugRepository.deleteById(id);
+	}
+}
