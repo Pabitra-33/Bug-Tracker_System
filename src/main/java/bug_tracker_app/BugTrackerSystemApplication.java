4f218aa2557e3759ddf5f509756f6fe2137f0a8f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-10 19:04:12 +0530

    initial commit

---
+++ src/main/java/bug_tracker_app/BugTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package bug_tracker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bug_tracker_app/BugTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bug_tracker_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
