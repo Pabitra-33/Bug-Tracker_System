458700799e7743c6670fea1d1af050686f0c6d07
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-10 21:03:42 +0530

    created a service layer to wite the business logic

---
+++ src/main/java/bug_tracker_app/service/BugService.java
@@ -0,0 +1,14 @@
+package bug_tracker_app.service;
+
+import java.util.List;
+
+import bug_tracker_app.entity.Bug;
+
+public interface BugService {
+
+	//created abstract methods to achieve abstraction
+	public Bug addBug(Bug bug);//to add new bugs
+	public List<Bug> getAllBugs();//to get all bugs
+	public Bug updateBug(Long id, Bug bug);//to update existing bugs
+	public void deleteBug(Long id);//to delete bugs
+}
