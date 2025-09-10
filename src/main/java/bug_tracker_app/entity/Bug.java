2a6f1d7332a53eb5ce75e933834a42334b57ac2e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-10 19:38:16 +0530

    created the bug entity class to store new bug logs, update and delete

---
+++ src/main/java/bug_tracker_app/entity/Bug.java
@@ -0,0 +1,29 @@
+package bug_tracker_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bugs_info")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bug {
+
+	private Long id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status; // OPEN, IN_PROGRESS, RESOLVED, CLOSED
+}
+
+enum Status {
+    OPEN, IN_PROGRESS, RESOLVED, CLOSED
+}
