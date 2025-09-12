76ca851a2936618395e78febe6d9edd5b88f6839
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-12 09:45:50 +0530

    Updated all the rest api end-points for the CRUD operations in the controller

--- src/main/java/bug_tracker_app/controller/BugController.java
+++ src/main/java/bug_tracker_app/controller/BugController.java
@@ -3,8 +3,12 @@ package bug_tracker_app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +27,29 @@ public class BugController {
 	}
 	
 	
+	//CRUD operation methods
 	@PostMapping
 	public Bug createBug(Bug bug) {
 		return bugService.addBug(bug);
 	}
 	
 	
+	
 	@GetMapping
 	public List<Bug> getAllBugs(){
 		return bugService.getAllBugs();
+	}
+	
+	
+	@PutMapping("/{id}")
+	public Bug updateBug(@PathVariable Long id, @RequestBody Bug bug) {
+		return bugService.updateBug(id, bug);
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public String deleteBug(@PathVariable Long id) {
+		bugService.deleteBug(id);
+		return "Bug deleted successfully";
 	}
 }
